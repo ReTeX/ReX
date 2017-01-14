ed78e5d25895fb243cb7f437f14dd843ca39cfa8
cbreeden <github@u.breeden.cc>
2017-01-14 01:26:41 -0600

    Try placing glyphs in array

--- src/font/mod.rs
+++ src/font/mod.rs
@@ -17,9 +17,8 @@ pub use self::offsets::OptionalAtom;
 use parser::AtomType;
 
 pub fn glyph_metrics(code : u32) -> Glyph {
-    GLYPHS.get(&code)
+    **GLYPHS.get(&code)
         .expect(&format!("Unable to find glyph for code {}", code))
-        .clone()
 }
 
 #[allow(dead_code)]
@@ -162,6 +161,6 @@ impl Default for Family {
 
 impl Default for Weight {
     fn default() -> Weight {
-        Weight::None
+        Weight::Italic
     }
 }
