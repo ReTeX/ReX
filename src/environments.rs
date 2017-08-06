4eaaaf46d790f976967d4dac2eae5ef61cb941aa
Christopher Breeden <github@u.breeden.cc>
2017-08-06 16:59:06 -0500

    being matrix support

--- src/environments.rs
+++ src/environments.rs
@@ -6,6 +6,8 @@ use parser::optional_argument_with;
 use parser::required_argument_with;
 use parser::ParseNode;
 use error::{Result, Error};
+use font::Symbol;
+use font::AtomType;
 
 /// An enumeration of recognized enviornmnets.
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
@@ -39,7 +41,12 @@ impl Environment {
     pub fn parse(&self, lex: &mut Lexer, local: Style) -> Result<ParseNode> {
         match *self {
             Environment::Array => array(lex, local),
-            _ => Err(Error::Todo),
+            Environment::Matrix => matrix(lex, local),
+            Environment::PMatrix => matrix_with(lex, local, '(', ')'),
+            Environment::BMatrix => matrix_with(lex, local, '[', ']'),
+            Environment::BbMatrix => matrix_with(lex, local, '{', '}'),
+            Environment::VMatrix => matrix_with(lex, local, '|', '|'),
+            Environment::VvMatrix => matrix_with(lex, local, '\u{2016}', '\u{2016}'),
         }
     }
 }
@@ -69,7 +76,7 @@ impl Default for ArrayVerticalAlign {
 /// Array column alignent.  These are parsed as a required macro argument
 /// for the array enviornment. The default value is `Centered`.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum ArrayColumnAlign  {
+pub enum ArrayColumnAlign {
     /// Column is centered
     Centered,
 
@@ -116,8 +123,55 @@ pub struct Array {
 
     /// A collection of rows.  Each row consists of one `Vec<Expression>`.
     pub rows: Vec<Vec<Expression>>,
+
+    /// The left delimiter for the array (optional).
+    pub left_delimiter: Option<Symbol>,
+
+    /// The right delimiter for the array (optional).
+    pub right_delimiter: Option<Symbol>,
 }
 
+
+fn matrix(lex: &mut Lexer, style: Style) -> Result<ParseNode> {
+    matrix_common(lex, style, None, None)
+}
+
+fn matrix_with(lex: &mut Lexer,
+               style: Style,
+               left_delimiter: char,
+               right_delimiter: char)
+               -> Result<ParseNode> {
+    matrix_common(lex, style, Some(left_delimiter), Some(right_delimiter))
+}
+
+fn matrix_common(lex: &mut Lexer,
+                 style: Style,
+                 left_delimiter: Option<char>,
+                 right_delimiter: Option<char>)
+                 -> Result<ParseNode> {
+    // matrix bodies are paresed like arrays.
+    let body = array_body(lex, style)?;
+    let left_delimiter = left_delimiter.map(|code| {
+                                                Symbol {
+                                                    unicode: code as u32,
+                                                    atom_type: AtomType::Inner,
+                                                }
+                                            });
+
+    let right_delimiter = right_delimiter.map(|code| {
+                                                  Symbol {
+                                                      unicode: code as u32,
+                                                      atom_type: AtomType::Inner,
+                                                  }
+                                              });
+
+    Ok(ParseNode::Array(Array {
+                            col_format: ArrayColumnsFormatting::default(),
+                            rows: body,
+                            left_delimiter,
+                            right_delimiter,
+                        }))
+}
 
 /// Parse the column alignments for arrays.  The currently supported formats are:
 ///   - `c` center the column
@@ -140,11 +194,11 @@ fn array_col(lex: &mut Lexer, _: Style) -> Result<ArrayColumnsFormatting> {
                 lex.next();
                 lex.consume_whitespace();
                 continue;
-            },
+            }
             Token::Symbol('}') => {
                 lex.pos -= 1; // backtrack the lexer
                 break;
-            },
+            }
             _ => return Err(Error::Todo),
         }
 
@@ -156,9 +210,9 @@ fn array_col(lex: &mut Lexer, _: Style) -> Result<ArrayColumnsFormatting> {
     }
 
     Ok(ArrayColumnsFormatting {
-        columns: cols,
-        right_vert: current.left_vert,
-    })
+           columns: cols,
+           right_vert: current.left_vert,
+       })
 }
 
 /// Parse the optional argument in an array enviornment.  This dictates the
@@ -186,7 +240,7 @@ fn array_pos(lex: &mut Lexer, _: Style) -> Result<Option<ArrayVerticalAlign>> {
 /// space between the rows.  Note, the last line termination is ignored
 /// if the a line is empty.
 type Expression = Vec<ParseNode>;
-fn array_contents(lex: &mut Lexer, style: Style) -> Result<Vec<Vec<Expression>>> {
+fn array_body(lex: &mut Lexer, style: Style) -> Result<Vec<Vec<Expression>>> {
     let mut rows: Vec<Vec<Expression>> = Vec::new();
     let mut current: Vec<Expression> = Vec::new();
     loop {
@@ -194,22 +248,23 @@ fn array_contents(lex: &mut Lexer, style: Style) -> Result<Vec<Vec<Expression>>>
         if lex.current == Token::Command(r"end") {
             // If the last line is empty, ignore it.
             if expr.is_empty() && current.is_empty() {
-                break
+                break;
             }
 
             current.push(expr);
             rows.push(current);
-            break
+            break;
         }
 
         current.push(expr);
         match lex.current {
-            Token::Symbol('&') => { /* no-op, carry on */ },
-            Token::Command(r"\") | Token::Command(r"cr") => {
+            Token::Symbol('&') => { /* no-op, carry on */ }
+            Token::Command(r"\") |
+            Token::Command(r"cr") => {
                 // TODO: Handle space arguments here.
                 rows.push(current);
                 current = Vec::new();
-            },
+            }
             _ => panic!("Unexpected expression end: {:?}", lex.current),
         }
         lex.next();
@@ -220,14 +275,16 @@ fn array_contents(lex: &mut Lexer, style: Style) -> Result<Vec<Vec<Expression>>>
 /// Parse an array environment.  This method assumes that the lexer is currently
 /// positioned after the `\begin{array}` declaration.
 fn array(lex: &mut Lexer, local: Style) -> Result<ParseNode> {
-    let pos  = optional_argument_with(lex, local, array_pos)?;
+    let pos = optional_argument_with(lex, local, array_pos)?;
     let cols = required_argument_with(lex, local, array_col)?;
     lex.next();
-    let contents = array_contents(lex, local)?;
+    let contents = array_body(lex, local)?;
     debug!("Array, pos: {:?}, cols: {:?}", pos, cols);
     debug!("Contents: {:#?}", contents);
     Ok(ParseNode::Array(Array {
-        col_format: cols,
-        rows: contents,
-    }))
+                            col_format: cols,
+                            rows: contents,
+                            left_delimiter: None,
+                            right_delimiter: None,
+                        }))
 }
